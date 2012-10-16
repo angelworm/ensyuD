f7b29b9c469c5607afabbd17a86110d89d9a913d
NAKAJIMA Susumu <ueda2008+git@gmail.com>
2012-10-16 15:49:02 +0900

    parser has compatible to 004.pas

--- src/jp/angeworm/ensyuD/Token.java
+++ src/jp/angeworm/ensyuD/Token.java
@@ -29,4 +29,41 @@ public class Token {
 		value = v;
 		lineNumber = linum;
 	}
+	public Token(TokenType tt, String v) {
+		type = tt;
+		value = v;
+		lineNumber = 0;
+	}
+	public Token(TokenType tt) {
+		type = tt;
+		value = "";
+		lineNumber = 0;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Token other = (Token) obj;
+		if (type != other.type)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
 }
